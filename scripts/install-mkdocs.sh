06e986168ab1d8f910b6ac9dd0002ccd95e1daef
cocvu99 <cocvu99@gmail.com>
2026-04-20 02:20:06 +0700

    fix CI pipeline

--- scripts/install-mkdocs.sh
+++ scripts/install-mkdocs.sh
@@ -8,5 +8,6 @@ pip install \
     mkdocs-git-authors-plugin \
     mkdocs-git-revision-date-localized-plugin \
     mkdocs-simple-hooks \
-    mkdocs-rss-plugin \
-    plugins/mkdocs-git-committers-plugin-2
+    mkdocs-rss-plugin 
+
+    # plugins/mkdocs-git-committers-plugin-2
